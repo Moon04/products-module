0eafdbc1aebc39104a4ae78650985fe56efc9530
Mona Mohamed <mona.abdelmohsen@robustastudio.com>
2022-03-12 18:11:25 +0200

    feat: add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Grommet } from 'grommet';
+import React from "react";
+import { Box, Button, Heading, Grommet } from "grommet";
+import { Add } from "grommet-icons";
 
-import { theme } from './theme';
+import { theme } from "./theme";
+
+const AppBar = (props: any) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
 
 function App() {
   return (
     <Grommet theme={theme}>
+      <AppBar>
+        <Heading level="3" margin="none">
+          My Products
+        </Heading>
+        <Button
+          title={"Add Product"}
+          icon={<Add color="#ffffff" />}
+          onClick={() => {
+            // TODO: redirect to product form
+          }}
+        />
+      </AppBar>
     </Grommet>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,13 @@
 export const theme = {
   global: {
+    colors: {
+      brand: "#45b467",
+      text: "#ffffff",
+    },
     font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
     },
   },
-}
+};
